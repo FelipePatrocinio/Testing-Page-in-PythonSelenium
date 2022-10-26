db73477104b9daecdb72d21f04a1c98b11a5473a
Felipe Patrocinio <115433597+FelipePatrocinio@users.noreply.github.com>
2022-10-25 23:28:55 -0300

    Update testeQa8.py

--- testeQa8.py
+++ testeQa8.py
@@ -4,18 +4,21 @@ from tkinter import PAGES
 from selenium import webdriver
 from time import sleep, time
 from PIL import Image 
+from selenium.webdriver.common.by import By
 
 driver = webdriver.Chrome() 
+
+### Test case #002 - Validando pagina de download ###
+
 driver.get("https://www.selenium.dev/")
 driver.maximize_window()
 sleep(2)
-driver.find_element('xpath', '//*[@id="main_navbar"]/ul/li[3]/a/span').click()
+driver.find_element(By.CSS_SELECTOR, 'a[href="/downloads"]').click()
 
 
 url = driver.current_url
 title = driver.title
 print(url,"\n", title)
-driver.get(url) 
 driver.save_screenshot("image.png") 
 image = Image.open("image.png") 
 image.show() 
